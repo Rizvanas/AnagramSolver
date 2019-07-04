b3581f2304e4d480afea8f8ae74ded4326d7b796
Rizvan <rizvanchalilovas@gmail.com>
2019-07-04 16:38:30 +0300

    Refactor tests

--- Implementation.Tests/AnagramSolverTests.cs
+++ Implementation.Tests/AnagramSolverTests.cs
@@ -11,22 +11,20 @@ namespace Implementation.Tests
     [TestFixture]
     public class AnagramSolverTests
     {
-        private readonly AnagramSolver _anagramSolver = new AnagramSolver(
-            new List<Word>
-            {
-                new Word { Text = "Labas", Type="bdv" },
-                new Word { Text = "Laibas", Type="bdv" },
-                new Word { Text = "Lakta", Type="daikt" },
-                new Word { Text = "Vilnius", Type="tikr. daikt" },
-                new Word { Text = "Miestas", Type="bdv" },
-                new Word { Text = "Silute", Type="bdv" },
-                new Word {Text = "Sula", Type="daikt" }
-            });
+
+        private readonly AnagramSolver _anagramSolver1 = new AnagramSolver
+            (new TxtWordLoader { FilePath = @"..\AnagramGenerator\testzodynas1.txt" }.Load().ToList());
+
+        private readonly AnagramSolver _anagramSolver2 = new AnagramSolver
+            (new TxtWordLoader { FilePath = @"..\AnagramGenerator\testzodynas2.txt" }.Load().ToList());
+
+        private readonly AnagramSolver _anagramSolver3 = new AnagramSolver
+            (new TxtWordLoader { FilePath = @"..\AnagramGenerator\testzodynas3.txt" }.Load().ToList());
 
         [Test]
         public void GetAnagrams_WordContainsLithuanianChars_ResultCountIs0()
         {
-            var testAnagrams = _anagramSolver.GetAnagrams("Šilutė");
+            var testAnagrams = _anagramSolver1.GetAnagrams("Šilutė");
 
             Assert.IsEmpty(testAnagrams);
         }
@@ -34,7 +32,7 @@ namespace Implementation.Tests
         [Test]
         public void GetAnagrams_WordIsSmallerThan5_ResultCountIs0 ()
         {
-            var testAnagrams = _anagramSolver.GetAnagrams("Šilutė");
+            var testAnagrams = _anagramSolver1.GetAnagrams("Šilutė");
 
             Assert.IsEmpty(testAnagrams);
         }
@@ -42,7 +40,7 @@ namespace Implementation.Tests
         [Test]
         public void GetAnagrams_WordValueIsAlus_ResultCountIs1()
         {
-            var testAnagrams = _anagramSolver.GetAnagrams("Alus");
+            var testAnagrams = _anagramSolver1.GetAnagrams("Alus");
 
             Assert.AreEqual(testAnagrams.Count, 1);
         }
@@ -50,7 +48,7 @@ namespace Implementation.Tests
         [Test]
         public void GetAnagrams_WordValueIsAlus_ResultIsSula()
         {
-            var testAnagrams = _anagramSolver
+            var testAnagrams = _anagramSolver1
                 .GetAnagrams("Alus").Select(a => String.Join(' ', a.Select(t => t.Text)))
                 .ToList();
 
@@ -60,7 +58,7 @@ namespace Implementation.Tests
         [Test]
         public void GetAnagrams_WordValueIsEmptyString_ResultIsEmptyString()
         {
-            var testAnagrams = _anagramSolver
+            var testAnagrams = _anagramSolver1
                 .GetAnagrams("").Select(a => String.Join(' ', a.Select(t => t.Text)))
                 .ToList();
 
@@ -70,7 +68,7 @@ namespace Implementation.Tests
         [Test]
         public void GetAnagrams_WordValueIsNull_ResultCOuntIs0()
         {
-            Assert.That(() => _anagramSolver.GetAnagrams(null), Throws.ArgumentNullException);
+            Assert.That(() => _anagramSolver1.GetAnagrams(null), Throws.ArgumentNullException);
         }
 
     }
